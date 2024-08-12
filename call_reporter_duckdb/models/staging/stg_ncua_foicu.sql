2ece783b967dea2fb4fc7149dcd8591b492fca84
michael_herman <m.herman.1000@gmail.com>
2024-08-12 14:56:09 -0500

    Removed demo jaffle shop junk.

--- call_reporter_duckdb/models/staging/stg_ncua_foicu.sql
+++ call_reporter_duckdb/models/staging/stg_ncua_foicu.sql
@@ -1,11 +1,6 @@
 with source as (
 
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
     select * from {{source('call_reporter','foicu')}}
-    -- select * from {{ ref('raw_customers') }}
 
 ),
 
---
+++ superset/assets/superset_config.py
@@ -0,0 +1,5 @@
+# Secret Key
+SECRET_KEY = 'SHHH_ITS_A_SECRET'
+
+# Superset specific config
+# ROW_LIMIT = 5000
